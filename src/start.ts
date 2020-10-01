f373dd6a833844c38875eb569ca333eadca2e25a
Eden <eg7eg7@gmail.com>
2020-10-01 18:34:37 +0300

    /networks

--- src/start.ts
+++ src/start.ts
@@ -1,10 +1,32 @@
 var express = require("express");
+const { networkInterfaces } = require("os");
+const nets = networkInterfaces();
+function getNetworks(): any {
+  const results = Object.create(null); // or just '{}', an empty object
 
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+      // skip over non-ipv4 and internal (i.e. 127.0.0.1) addresses
+      if (net.family === "IPv4" && !net.internal) {
+        if (!results[name]) {
+          results[name] = [];
+        }
+
+        results[name].push(net.address);
+      }
+    }
+  }
+  return results;
+}
 export default function createServer() {
   var app = express();
 
   app.get("/", (req: any, res: { send: (arg0: string) => any }) =>
-    res.send("Service 1 - Update 1")
+    res.send("Service 1 - Update 2")
+  );
+
+  app.get("/networks", (req: any, res: { send: (arg0: string) => any }) =>
+    res.send(getNetworks())
   );
 
   return app;
